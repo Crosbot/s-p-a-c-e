58c962fe0ad19fded4763fd6f22495515df0df21
Michael Crosby <xx>
2013-04-01 00:31:27 +0100

    Implemented Camera and ship

---
+++ s-p-a-c-e/advCamera/Camera.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace advCamera
+{
+    public interface IFocusable
+    {
+        Vector2 Position { get; }
+        Vector2 WidthHeight { get; }
+        Vector2 Origin { get; }
+    }
+
+    class Camera
+    {
+        private Matrix transform;
+        public Matrix Transform
+        {
+            get { return transform; }
+        }
+
+
+        private Vector2 centre;
+        private Viewport viewport;
+
+        private float zoom = 1;
+        private float rotation = 0;
+
+        public float x
+        {
+            get { return centre.X; }
+            set { centre.X = value; }
+        }
+        public float y
+        {
+            get { return centre.Y; }
+            set { centre.Y = value; }
+        }
+
+        public float Zoom
+        {
+            get { return zoom; }
+            set { zoom = value; if (zoom < 0.1f) zoom = 0.1f;}
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+        public Camera(Viewport newViewport)
+        {
+            viewport = newViewport;
+        }
+
+        public void Update(IFocusable focusable)
+        {
+            
+            centre = new Vector2(focusable.Position.X, focusable.Position.Y);
+            transform = Matrix.CreateTranslation(new Vector3(-centre.X, -centre.Y, 0)) *
+                        Matrix.CreateRotationZ(Rotation) *
+                        Matrix.CreateScale(new Vector3(Zoom, Zoom, 0)) *
+                        Matrix.CreateTranslation(new Vector3(viewport.Width / 2, viewport.Height / 2, 0));
+
+        }
+    }
+}
---
+++ s-p-a-c-e/advCamera/Ship.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace advCamera
+{
+    
+    class Ship : IFocusable
+    {
+        Texture2D texture;
+        Vector2 position;
+        Vector2 widthHeight;
+        Vector2 origin; 
+
+
+        public Vector2 Position
+        {
+            get { return position; }
+        }
+
+        public Vector2 WidthHeight
+        {
+            get { return widthHeight; }
+        }
+
+        public Vector2 Origin
+        {
+            get { return origin; }
+        }
+
+        public Ship()
+        {
+        }
+
+        float moveSpeed = 5f;
+        float rotationAngle = 0;
+        public void Update()
+        {
+            position.X += GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X * moveSpeed;
+            position.Y += (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y * moveSpeed) * -1;
+             rotationAngle = (float)Math.Atan2(GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X, GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y);
+
+        }
+
+        public void Load(ContentManager Content)
+        {
+            texture = Content.Load<Texture2D>("ship");
+            position = new Vector2(100f, 100f);
+            widthHeight = new Vector2(texture.Width, texture.Height);
+            Debug.WriteLine(texture.Width);            
+            origin.X += (float)texture.Width / 2;
+            origin.Y += (float)texture.Height / 2;
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, null, Color.White, rotationAngle,origin, 1.0f, SpriteEffects.None, 0f);
+        }
+    }
+}
